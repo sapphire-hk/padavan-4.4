6cd443ec489709cb7c4c716569a8b54d7b3ef019
sapphire-hk <56421467+sapphire-hk@users.noreply.github.com>
2026-04-16 00:00:23 +0800

    Update board.mk

--- trunk/configs/boards/K2P-USB/board.mk
+++ trunk/configs/boards/K2P-USB/board.mk
@@ -5,5 +5,5 @@
 ##################################################################
 
 CFLAGS += -DBOARD_K2P -DBOARD_MT7615_DBDC
-BOARD_NUM_USB_PORTS=1
-CONFIG_BOARD_RAM_SIZE=128
+BOARD_NUM_USB_PORTS=2
+CONFIG_BOARD_RAM_SIZE=512
